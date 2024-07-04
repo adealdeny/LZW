fc64856177e19e4c1a7eac3d778f39121f33504a
adealdeny <adealdeny@gmail.com>
2024-07-05 00:28:36 +0800

    2024-07-05 00:28:36

--- main.py
+++ main.py
@@ -200,3 +200,4 @@ const handleEvents = async (events = []) => (
       (await Promise.all(
         events
           .map((event) => new Event(event))
+          .filter((event) => event.isMessage)
