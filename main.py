cf5db05ac0ce4e3ef901f95be9fdccff1a8aceb4
adealdeny <adealdeny@gmail.com>
2024-07-08 21:05:07 +0800

    2024-07-08 21:05:07

--- main.py
+++ main.py
@@ -207,3 +207,4 @@ const handleEvents = async (events = []) => (
       ))
         .map((context) => (context.error ? context : handleContext(context))),
     ))
+      .filter((context) => context.messages.length > 0)
