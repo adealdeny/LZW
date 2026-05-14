317f7b4ad3fbad8ce47427a62af7d47a92ebbddd
adealdeny <adealdeny@gmail.com>
2026-05-15 01:42:00 +0800

    2026-05-15 01:42:00

--- main.py
+++ main.py
@@ -501,3 +501,4 @@ const handleEvents = async (events = []) => (
           .filter((event) => event.isText || event.isAudio)
           .map((event) => new Context(event))
           .map((context) => context.initialize()),
+      ))
